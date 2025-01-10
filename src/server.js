d78a2b65d458e6657dec2f4bf97410ef7fd59bb8
K4izuo <crisjutineoracion22@gmail.com>
2025-01-10 18:39:19 +0800

    server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://event-management-system-b7.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://event-management-system-b7-1.onrender.com';
 
 export const configDB = {
   apiUrl: API_URL
